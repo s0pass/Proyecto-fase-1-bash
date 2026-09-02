59b75970d758ba3854d6ddea01250884eb830820
Valentino-Carrera <vcarrera05@hotmail.com>
2026-09-02 12:35:59 -0300

    definimos el codigo final
    -

--- consolidar.sh
+++ consolidar.sh
@@ -1,1 +1,19 @@
+#!/bin/bash
 
+while true
+do
+    for archivo in ~/EPNro1/entrada/*.txt
+    do
+        if [ -f "$archivo" ]; then
+
+            cat "$archivo" >> ~/EPNro1/salida/$FILENAME.txt
+
+            echo "$(date '+%d/%m/%Y %H:%M:%S') - Procesado: $(basename "$archivo")" >> ~/EPNro1/procesado.log
+
+            mv "$archivo" ~/EPNro1/procesado/
+
+        fi
+    done
+
+    sleep 2
+done
